cd7eaf8a8976e8922d1ad390e1aee635468f5f07
Jeffrey Chang <you@example.com>
2019-11-24 19:34:06 -0500

    nuked

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-//import express from 'express';
-const express=require("express")
+
+import express=require('express');
+import bodyParser = require('body-parser');
+import {loggingMiddleware} from './middleware/loggingMiddleware';
+import {sessionMiddleware} from './middleware/sessionMiddleware';
 
 const app=express();
-const port=1234;
+const port=5432;
 
-app.get('/',(req,res)=>{
-    res.send("please log in");
-});
+app.use(bodyParser.json());
+app.use(loggingMiddleware);
+app.use(sessionMiddleware);
+
+//app.use('/users',userRouter)
+
+//app.use('/report',reportRouter)
+//app.use('/login',loginRouter)
+
 
 app.listen(port,()=>{
     console.log("here we go")// http://localhost1234/ has started
