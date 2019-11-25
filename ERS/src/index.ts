83afbdac5d5836fdbb3a1bd1e4a8aefce254b1bf
Jeffrey Chang <you@example.com>
2019-11-24 22:21:29 -0500

    more than 10% maybe

--- ERS/src/index.ts
+++ ERS/src/index.ts
@@ -8,10 +8,16 @@ app.use(bodyparser.json());
 
 app.use(sessionMiddleware)
 
-app.post('/login',(req,res)=>{
+app.post('/login',async(req,res)=>{
     const {username,password} = req.body;
     if (!username || !password){
         res.status(400).send('please have a username and password field')
     }
-    
+    try {
+        
+        
+    } catch (e) {
+        
+    }
+
 })
